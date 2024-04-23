75a854bf881df5326d713677f72fc5d996fd83b1
James Kariuki <nyamuj59@gmail.com>
2024-04-24 01:46:02 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+//middleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+//routes
+
+app.use('/api/v1/tasks', tasks)
+
+const port = process.env.PORT
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server is running on port ${port}...`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
